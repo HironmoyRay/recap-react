c9548ed9e08106d11be88c7260eb87912068751d
hironmoy-ray <hpray35@gmail.com>
2024-08-15 03:47:36 +0600

    selected State

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -5,6 +5,17 @@ import './Countries.css'
 const Countries = () => {
 
   const [countries, setCountries] = useState([]);
+    const [selectedCountries, setSelectedCountries] = useState([]);
+
+    console.log(selectedCountries.length);
+    
+
+    const handleSelectedCountries = (country)=>{
+        // console.log(`${country.name.common} Visited `);
+        const newSelecetCountries = [...selectedCountries, country];
+        setSelectedCountries(newSelecetCountries);
+        
+    }
 
   useEffect(()=>{
     fetch("https://restcountries.com/v3.1/all")
@@ -15,9 +26,17 @@ const Countries = () => {
   return (
     <div>
       <h1>Countries: {countries.length}</h1>
+      <div>
+        <h2>Selected Countries: {selectedCountries.length}</h2>
+        <ul>
+            {
+                selectedCountries.map(selected => <li key={selected.car.cca2}>{selected.name.common}</li>)
+            }
+        </ul>
+      </div>
       <div className="country-container">
       {
-        countries.map((country)=> <Country key={country.car.cca2} country={country}></Country>)
+        countries.map((country)=> <Country key={country.car.cca2} country={country} handleSelectedCountries={handleSelectedCountries}></Country>)
       }
       </div>
     </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,24 +1,36 @@
-import {useState} from 'react'
+import { useState } from "react";
+import "./Country.css";
 
-const Country = ({country}) => {
-    console.log(country);
-    const {name, flags, capital} = country;
-    const [visited,setVisited] = useState(false);
-    const handleVisited = () =>{
-        setVisited(!visited)
-    }
-    return (
-        <div className="box">
-            <h1>{name.common}</h1>
-            <hr />
-            <div>
-                <img src={flags.png} alt="" />
-            </div>
-            <h2>{capital}</h2>
-            <button onClick={handleVisited}>{visited?"Visited":"Going"}</button>
-
-        </div>
-    );
+const Country = ({ country, handleSelectedCountries }) => {
+  //   console.log(country);
+  const { name, flags, capital } = country;
+  const [visited, setVisited] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const handleSelected = () => {
+    setSelected(!selected);
+  };
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+  return (
+    <div className="box">
+      <h1>{name.common}</h1>
+      <hr />
+      <div className="img-div">
+        <img src={flags.png} alt="" />
+      </div>
+      <h2>{capital}</h2>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      <button
+        onClick={() => {
+          handleSelectedCountries(country);
+          handleSelected();
+        }}
+      >
+        {selected ? "Selected" : "Select"}
+      </button>
+    </div>
+  );
 };
 
 export default Country;
